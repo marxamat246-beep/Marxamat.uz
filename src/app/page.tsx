bb2c27d87b579321afb95e3ccf1d348eeb054f75
marxamat246-beep <marxamat246@gmail.com>
2026-05-07 15:58:22 +0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      fontSize: '48px', 
+      fontWeight: 'bold',
+      fontFamily: 'sans-serif' 
+    }}>
+      MARXAMAT.UZ
+    </main>
+  );
+}
